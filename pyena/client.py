32332f1cbd80b691b96bb546e2bfca19744d3b55
Sam Nicholls <samstudio8@gmail.com>
2020-05-19 18:48:12 +0000

    return the formal name in platform trans

--- pyena/client.py
+++ pyena/client.py
@@ -50,9 +50,9 @@ def _convert_platform(instrument_name):
 
     instrument_name = instrument_name.replace('_', ' ').lower()
     for instrument_make, instrument_models in valid_enums.items():
-        for possible_model in instrument_models:
-            if possible_model.lower() in instrument_name:
-                return instrument_make, possible_model
+        for possible_model_k, possible_model_v in instrument_models:
+            if possible_model_k.lower() in instrument_name:
+                return instrument_make, possible_model_v
     return None, None
 
 def _add_today(center_name):
